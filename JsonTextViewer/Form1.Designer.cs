ae67c6d7b12b6b6458138a47e57f3bbf42eb05e4
rsbabu1092 <117066662+rsbabu1092@users.noreply.github.com>
2022-10-31 13:18:26 +0530

    add json files this location

---
+++ JsonTextViewer/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace JsonTextViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFileChooser = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.dgJsonText = new System.Windows.Forms.DataGridView();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgJsonText)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFileChooser
+            // 
+            this.btnFileChooser.Location = new System.Drawing.Point(543, 43);
+            this.btnFileChooser.Name = "btnFileChooser";
+            this.btnFileChooser.Size = new System.Drawing.Size(75, 23);
+            this.btnFileChooser.TabIndex = 0;
+            this.btnFileChooser.Text = "Choose";
+            this.btnFileChooser.UseVisualStyleBackColor = true;
+            this.btnFileChooser.Click += new System.EventHandler(this.btnFileChooser_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(73, 45);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(464, 20);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Path";
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(246, 86);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(179, 23);
+            this.btnReadFile.TabIndex = 0;
+            this.btnReadFile.Text = "Generate DatGrid";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // dgJsonText
+            // 
+            this.dgJsonText.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgJsonText.Location = new System.Drawing.Point(22, 141);
+            this.dgJsonText.Name = "dgJsonText";
+            this.dgJsonText.Size = new System.Drawing.Size(677, 297);
+            this.dgJsonText.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(624, 43);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 450);
+            this.Controls.Add(this.dgJsonText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFileChooser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JSON File Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.dgJsonText)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFileChooser;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.DataGridView dgJsonText;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ JsonTextViewer/Form1.cs
@@ -0,0 +1,188 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace JsonTextViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFileChooser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Title = "Browse JSON Files";
+
+            openFileDialog1.Filter = "Json files (*.json)|*.json|Text files (*.txt)|*.txt";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtFilePath.Text.Trim()))
+            {
+                using (StreamReader r = new StreamReader(txtFilePath.Text.Trim()))
+                {
+                    string jsonText = r.ReadToEnd();
+
+                    if (!string.IsNullOrEmpty(jsonText))
+                    {
+                        dgJsonText.DataSource = GenerateDataTable(jsonText);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No file found !", "Json Viewer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFilePath.Text = string.Empty;
+
+            dgJsonText.DataSource = null;
+
+            txtFilePath.Focus();
+        }
+
+        private DataTable GenerateDataTable(string json)
+        {
+            DataTable dtResult = new DataTable();
+
+            try
+            {
+                var ParsedText = JObject.Parse(json);
+
+                string ResourceType = ParsedText["resourceType"].ToString();
+                string Type = ParsedText["type"].ToString();
+
+                dtResult.Columns.AddRange(new[]
+                    {
+                    new DataColumn("URN"),
+                    new DataColumn("Gender"),
+                    new DataColumn("Age"),
+                    new DataColumn("Race"),
+                    new DataColumn("Ethinicity"),
+                    new DataColumn("MotherMaidenName"),
+                    new DataColumn("DeceasedDateTime"),
+                    new DataColumn("Address"),
+                    });
+
+                var EntryNodeVal = ParsedText["entry"];
+
+                EntryNodeVal.All(entryVal =>
+                {
+                    dtResult.Rows.Add(entryVal["fullUrl"] == null ? "" : (entryVal["fullUrl"].ToString()).Replace("urn:uuid:", ""));
+
+                    return true;
+                });
+
+                dtResult.AcceptChanges();
+
+                DataRow dRow = null;
+
+                EntryNodeVal.All(entryVal =>
+                {
+                    string URN = entryVal["fullUrl"] == null ? "" : (entryVal["fullUrl"].ToString()).Replace("urn:uuid:", "");
+
+                    dRow = dtResult.Select("URN = '" + URN + "'")[0];
+
+                    var resourceValue = JObject.Parse(entryVal["resource"].ToString());
+
+                    dRow["Gender"] = resourceValue["gender"] == null ? "" : resourceValue["gender"].ToString();
+                    dRow["DeceasedDateTime"] = resourceValue["deceasedDateTime"] == null ? "" : resourceValue["deceasedDateTime"].ToString();
+
+                    var Extension = JsonConvert.SerializeObject(resourceValue["extension"]);
+
+                    if (Extension != "null")
+                    {
+                        var ExtnArray = JArray.Parse(Extension);
+
+                        ExtnArray.All(ExtnValue =>
+                        {
+                            var Extension1 = JsonConvert.SerializeObject(ExtnValue["extension"]);
+                            var ValueString = JsonConvert.SerializeObject(ExtnValue["valueString"]);
+                            var ValueAddress = JsonConvert.SerializeObject(ExtnValue["valueAddress"]);
+                            var ValueDecimal = JsonConvert.SerializeObject(ExtnValue["valueDecimal"]);
+
+                            if (Extension1 != null && Extension1 != "null")
+                            {
+                                var ExtnArray1 = JArray.Parse(Extension1);
+
+                                if (ExtnArray1 != null)
+                                {
+                                    dRow["Race"] = ExtnArray1[1]["valueString"] == null ? "" : ExtnArray1[1]["valueString"].ToString();
+                                }
+                            }
+                            else if (ValueString != null && ValueString != "null")
+                            {
+                                if (JsonConvert.SerializeObject(ExtnValue["url"]).Contains("mother"))
+                                {
+                                    dRow["MotherMaidenName"] = JsonConvert.DeserializeObject(ValueString);
+                                }
+                                else
+                                {
+                                    dRow["Ethinicity"] = JsonConvert.DeserializeObject(ValueString);
+                                }
+                            }
+                            else if (ValueAddress != null && ValueAddress != "null")
+                            {
+                                var Addr = JObject.Parse(JsonConvert.DeserializeObject(ValueAddress).ToString());
+
+                                if (Addr != null)
+                                {
+                                    dRow["Address"] = Addr["city"].ToString() + ", " + Addr["state"].ToString() + ", " + Addr["country"].ToString();
+                                }
+                            }
+                            else if (ValueDecimal != null && ValueDecimal != "null")
+                            {
+                                if (JsonConvert.SerializeObject(ExtnValue["url"]).Contains("quality"))
+                                {
+                                    dRow["Age"] = JsonConvert.DeserializeObject(Math.Round(Convert.ToDouble(ValueDecimal)).ToString());
+                                }
+                            }
+
+                            return true;
+                        });
+
+                        string Identifier = resourceValue["identifier"].ToString();
+                        string Name = resourceValue["name"].ToString();
+                        string Telecom = resourceValue["telecom"].ToString();
+                        string MaritalStatus = resourceValue["maritalStatus"].ToString();
+                        string Communication = resourceValue["communication"].ToString();
+
+                        var request = JsonConvert.DeserializeObject<JToken>(entryVal["request"].ToString());
+
+                        request.All(requestValue =>
+                        {
+                            return true;
+                        });
+                    }
+
+                    dtResult.AcceptChanges();
+
+                    return true;
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Json Viewer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return dtResult;
+        }
+    }
+}
